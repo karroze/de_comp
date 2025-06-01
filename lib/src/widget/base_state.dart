009ea07dab14db3eea455929d682c5ec594b1d44
karroze <karroze.dev@gmail.com>
2025-06-01 11:56:28 +0300

    feat: removed automatic `.addToDisposable` for a Bloc inside BaseState; bumped version to `v0.0.6`

--- lib/src/widget/base_state.dart
+++ lib/src/widget/base_state.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 /// Base state to implement disposing and ease bloc acquisition.
 abstract class BaseState<T extends StatefulWidget> extends State<T> with DisposableHolderMixin {
   /// Reads [BLOC] from context
-  BLOC bloc<BLOC extends BaseBloc<Object, Object, Object, Object>>() => context.read<BLOC>()..addToDisposableHolder(disposableHolder);
+  BLOC bloc<BLOC extends BaseBloc<Object, Object, Object, Object>>() => context.read<BLOC>();
 
   @override
   void dispose() {
